241ed51f1673201edc35dda33c3feca1a3ed1ef5
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2020-07-28 23:21:15 +0100

    completing first version of user service

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask,jsonify
 
 
 app= Flask(__name__)
 
 #set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_config=os.getenv('APP_SETTINGS') 
+app.config.from_object(app.config)
 
 @app.route('/users/ping')
 def ping_pong():
